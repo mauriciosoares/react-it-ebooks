ae15c4ab1179fe6474891e5b9e628e82715dd51e
Mauricio Soares <m.soares2603@gmail.com>
2015-08-25 11:18:53 -0300

    new function syntax for object methods

--- app/components/App.js
+++ app/components/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Search from './Search'
 
 let App = React.createClass({
-  render: () => {
-    return <div>teste</div>
+  render() {
+    return <div>teste2</div>
   }
 });
 
