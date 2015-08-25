e7a5e2c4ff430e5b7d5afce6998f4a1c1b80d7f1
Mauricio Soares <m.soares2603@gmail.com>
2015-08-25 15:07:37 -0300

    improving a bunch of things

--- gulp/config.js
+++ gulp/config.js
@@ -1,7 +1,6 @@
 var PROJECT_ROOT = require('path').resolve(__dirname).replace(/gulp/, '');
 
 var config = {
-  env: process.env.NODE_ENV,
   scripts: {
     src: PROJECT_ROOT + '/app/index.js',
     dest: {
--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,18 +1,19 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var babelify = require('babelify');
 var watchify = require('watchify');
 
 var config = require('../config.js').scripts;
-var env = require('../config.js').env;
 
 function scripts(watch) {
   var bundler, rebundle;
 
   bundler = browserify(config.src, {
     basedir: __dirname,
-    debug: !(env === 'production'),
+    debug: !(gutil.env.type === 'production'),
     cache: {},
     packageCache: {},
     fullPaths: watch
@@ -26,13 +27,17 @@ function scripts(watch) {
 
 
   rebundle = function() {
-    console.log('rebundle');
-    var stream = bundler.bundle();
-    stream.on('error', function(err) {
-      console.log('error');
-    });
-    stream = stream.pipe(source(config.dest.src));
-    return stream.pipe(gulp.dest(config.dest.path));
+    var updateStart = Date.now();
+
+    bundler.bundle()
+      .on('error', function(err) {
+        gutil.log(err.toString());
+      })
+      .pipe(source(config.dest.src))
+      .pipe(gulp.dest(config.dest.path));
+
+    gutil.log('Code updated in', gutil.colors.cyan((Date.now() - updateStart) + 'ms'));
+
   };
 
   bundler.on('update', rebundle);
