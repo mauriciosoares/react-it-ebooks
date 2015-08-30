341ebd8dfc64bcef05370951ed6d582db7317d62
Mauricio Soares <m.soares2603@gmail.com>
2015-08-30 14:17:07 -0300

    changing src folder in gulps configuration

--- gulp/config.js
+++ gulp/config.js
@@ -3,7 +3,7 @@ var PROJECT_ROOT = __dirname.replace(/\/gulp/, '') + '/';
 var config = {
   PROJECT_ROOT: PROJECT_ROOT,
   scripts: {
-    src: PROJECT_ROOT + 'example/scripts/index.js',
+    src: PROJECT_ROOT + 'src/scripts/index.js',
     dest: {
       path: PROJECT_ROOT + 'build/js',
       src: 'bundle.js'
@@ -11,8 +11,8 @@ var config = {
   },
 
   sass: {
-    src: PROJECT_ROOT + 'example/sass/index.sass',
-    watch: PROJECT_ROOT + 'example/sass/**/*.sass',
+    src: PROJECT_ROOT + 'src/sass/index.sass',
+    watch: PROJECT_ROOT + 'src/sass/**/*.sass',
     dest: PROJECT_ROOT + 'build/css',
   }
 };
