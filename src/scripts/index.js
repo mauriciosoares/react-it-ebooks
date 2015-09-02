1679b069de8409149e646b559b7fc6cf406993ad
Mauricio Soares <m.soares2603@gmail.com>
2015-09-02 17:59:50 -0300

    start to modularize the components

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,34 +1,4 @@
 import React from 'react'
-import Reflux from 'reflux'
-import bookStore from './stores/book'
-import bookActions from './actions/book'
-import _ from 'lodash'
-
-
-var App = React.createClass({
-  mixins: [Reflux.connect(bookStore)],
-
-  componentWillUpdate() {
-
-  },
-
-  render() {
-    var books = this.state.books.map((book) => {
-      return (
-        <div key={book.ID}>
-          {book.Title}<br />
-          {book.Description}<br />
-          <img src={book.Image} />
-        </div>
-      )
-    })
-
-    return(
-      <div onClick={bookActions.updateAge}>
-        {books}
-      </div>
-    )
-  }
-});
+import App from './components/App'
 
 React.render(<App />, document.body);
